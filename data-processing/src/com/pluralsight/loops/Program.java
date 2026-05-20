e83687957c32ee35ad111033af9b582bde543509
Louie D <louie.delrosario97@gmail.com>
2026-05-20 12:17:20 -0700

    Created Person class with variables, getter/setters, and constructor. Created an arraylist 'name' and filled the arraylist with 10 Person objects

--- data-processing/src/com/pluralsight/loops/Program.java
+++ data-processing/src/com/pluralsight/loops/Program.java
@@ -7,22 +7,35 @@ public class Program
 {
     public static void main(String[] args)
     {
-//        ArrayList<String> name = new ArrayList<>();
-//        name.add("Gon Freecss");
-//        name.add("Killua Zoldyck");
-//        name.add("Kurapika Kurta");
-//        name.add("Leorio Paradinight");
-//        name.add("Hisoka Morow");
-//        name.add("Chrollo Lucilfer");
-//        name.add("Netero Isaac");
-//        name.add("Feitan Portor");
-//        name.add("Alluka Zoldyck");
-//        name.add("Machi Komacine");
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Search name: ");
-        String search = scanner.nextLine();
+
+        // Manually creating instances of 'Person' class.
+        Person gon = new Person("Gon", "Freecss", 12);
+        Person killua = new Person("Killua", "Zoldyck", 12);
+        Person kurapika = new Person("Kurapika", "Kurta", 17);
+        Person leorio = new Person("Leorio", "Paradinight", 19);
+        Person hisoka = new Person("Hisoka", "Morow", 28);
+        Person illumi = new Person("Illumi", "Zoldyck", 24);
+        Person chrollo = new Person("Chrollo", "Lucilfer", 26);
+        Person biscuit = new Person("Biscuit", "Krueger", 57);
+        Person ging = new Person("Ging", "Freecss", 32);
+        Person alluka = new Person("Alluka", "Zoldyck", 11);
+
+        // Creating an 'Person' Arraylist and calling the list 'name'
+        ArrayList<Person> name = new ArrayList<>();
+
+        // Manually adding the 'Person' objects to our 'name' ArrayList
+        name.add(gon);
+        name.add(killua);
+        name.add(kurapika);
+        name.add(leorio);
+        name.add(hisoka);
+        name.add(illumi);
+        name.add(chrollo);
+        name.add(biscuit);
+        name.add(ging);
+        name.add(alluka);
+    }
 
     }
-}
 
