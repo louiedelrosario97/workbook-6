172b1d6f1081ee9e573d2707518abb7be67fdcb0
Louie D <louie.delrosario97@gmail.com>
2026-05-20 12:06:40 -0700

    Created Person class with variables, getter/setters, and constructor.

---
+++ data-processing/src/com/pluralsight/loops/Program.java
@@ -0,0 +1,28 @@
+package com.pluralsight.loops;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Program
+{
+    public static void main(String[] args)
+    {
+//        ArrayList<String> name = new ArrayList<>();
+//        name.add("Gon Freecss");
+//        name.add("Killua Zoldyck");
+//        name.add("Kurapika Kurta");
+//        name.add("Leorio Paradinight");
+//        name.add("Hisoka Morow");
+//        name.add("Chrollo Lucilfer");
+//        name.add("Netero Isaac");
+//        name.add("Feitan Portor");
+//        name.add("Alluka Zoldyck");
+//        name.add("Machi Komacine");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Search name: ");
+        String search = scanner.nextLine();
+
+    }
+}
+
